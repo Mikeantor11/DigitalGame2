1f0e266768bf7d86de51030661e20605419be5c2
Michael Hudson <mhudson8@gmu.edu>
2015-02-03 20:05:05 -0500

    Trying to get the explosions working

--- js/main.js
+++ js/main.js
@@ -174,7 +174,11 @@ window.onload = function() {
             wolf.animations.play('biteRight', 10);
 
             if(!bite.isPlaying){
-                bite.play();    
+                bite.play();
+                exp1.play();
+        exp2.play();
+        exp3.play();
+        exp4.play();    
             }            
         }
 
@@ -184,10 +188,7 @@ window.onload = function() {
             }
         }*/
 
-        exp1.play();
-        exp2.play();
-        exp3.play();
-        exp4.play();
+
 
         //Having to wait for the boss to stop Roaring in order to move.
         if(!roar.isPlaying){
