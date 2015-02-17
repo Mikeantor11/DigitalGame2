4c053611d9ea3b46fabd97b4cb3ac814df4d86a3
Michael Hudson <mhudson8@gmu.edu>
2015-02-17 11:20:26 -0500

    Changing games to Digital 3

--- js/main.js
+++ js/main.js
@@ -13,143 +13,61 @@ window.onload = function () {
 
     "use strict";
 
-    var game = new Phaser.Game(600, 600, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
+    var game = new Phaser.Game(600, 800, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
 
     function preload() {
-        game.load.spritesheet('heartButton', 'assets/Art/heartSpritesheet.png');
-        game.load.image('cursorButton', 'assets/Art/cursorButton.png');
-        game.load.image('cardButton', 'assets/Art/cardButton.png');
-        game.load.image('chocolateButton', 'assets/Art/chocoButton.png');
-        game.load.image('roseButton', 'assets/Art/roseButton.png');
-        game.load.image('heart', 'assets/Art/heart_2.png');
-        game.load.image('kittens', 'assets/Art/kittensInABox.png');
-        game.load.image('lose', 'assets/Art/kittenWhyYouNoLove.png');
+        game.load.image('platform', 'assets/Art/platform.png');
+        game.load.spritesheet('police', 'assets/Art/policeOfficer.png', 16, 31);
     }
 
-    //Global Variables
-    var heartButton;
-    var heartTotal = 0;
-    var text;
-    var cursorButton;
-    var cardButton;
-    var chocolateButton;
-    var roseButton;
-    var cursorPrice = 10;
-    var cardPrice = 50;
-    var chocolatePrice = 100;
-    var rosePrice = 300;
-    var cursorCount = 0;
-    var cardCount = 0;
-    var chocolateCount = 0;
-    var roseCount = 0;
-    var heartText;
-    var cursorText;
-    var cardText;
-    var chocolateText;
-    var roseText;
-    var cursorText2;
-    var cardText2;
-    var chocolateText2;
-    var roseText2;
-    var kittens;
-    var love = 0;
-    var loveText;
+    var police;
+    var plat1;
+    var plat2;
+    var plat3;
+    var cursors;
 
     function create() {
-        var style = {font: "20px Arial", fill: "#FFFFFF", align: "center"};
-
-        heartButton = game.add.button(20, 20, 'heart', heartClick);
-        cursorButton = game.add.button(325, 0, 'cursorButton', cursorBuy);
-        cardButton = game.add.button(325, 50, 'cardButton', cardBuy);
-        chocolateButton = game.add.button(325, 100, 'chocolateButton', chocolateBuy);
-        roseButton = game.add.button(325, 150, 'roseButton', roseBuy);
-        heartText = game.add.text(20, 150, "Hearts: " + heartTotal, style);
-        cursorText = game.add.text(580, 20, cursorCount, style);
-        cardText = game.add.text(580, 70, cardCount, style);
-        chocolateText = game.add.text(580, 120, chocolateCount, style);
-        roseText = game.add.text(580, 170, roseCount, style);
-        cursorText2 = game.add.text(280, 20, cursorPrice, style);
-        cardText2 = game.add.text(280, 70, cardPrice, style);
-        chocolateText2 = game.add.text(280, 120, chocolatePrice, style);
-        roseText2 = game.add.text(280, 170, rosePrice, style);
-        kittens = game.add.button(0, 200, "kittens", feedKitens);
-        loveText = game.add.text(0, 180, "Show the Kittens your love: " + love, style);
-
-        game.time.events.loop(1000, updateHearts, this);
-        game.time.events.loop(2000, upTheLove, this);
-    }
-
-    function heartClick() {
-        heartTotal++;
-    }
-
-    function feedKitens() {
-        if (love >= 10 && heartTotal >= 10) {
-            love = love - 10;
-            heartTotal = heartTotal - 10;
-        }
-    }
-
-    function upTheLove() {
-       /* if ((cursorCount + cardCount + chocolateCount + roseCount) > 5) {
-            love = love + Math.floor(Math.random() * 20) + 1;
-        }
-        else{
-            love = love + Math.floor(Math.random() * 10) + 1;
-        }*/
-        love = love + Math.floor(Math.random() * ((cursorCount + cardCount + chocolateCount + roseCount))*5) + 1;
-    }
-
-    function updateHearts() {
-        heartTotal = heartTotal + ((0.5 * cursorCount) + (3 * cardCount) + (5 * chocolateCount) + (10 * roseCount));
-    }
-
-    function cursorBuy() {
-        if (heartTotal >= cursorPrice) {
-            cursorCount++;
-            heartTotal = heartTotal - cursorPrice;
-            cursorPrice = cursorPrice + 5;
-        }
-    }
-
-    function cardBuy() {
-        if (heartTotal >= cardPrice) {
-            cardCount++;
-            heartTotal = heartTotal - cardPrice;
-            cardPrice = cardPrice + 5;
-        }
-    }
-
-    function chocolateBuy() {
-        if (heartTotal >= chocolatePrice) {
-            chocolateCount++;
-            heartTotal = heartTotal - chocolatePrice;
-            chocolatePrice = chocolatePrice + 5;
-        }
-    }
-
-    function roseBuy() {
-        if (heartTotal >= rosePrice) {
-            roseCount++;
-            heartTotal = heartTotal - rosePrice;
-            rosePrice = rosePrice + 5;
-        }
+        
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.gravity.y = 200;
+        
+        police = game.add.sprite(100,67, 'police');
+        plat1 = game.add.sprite(75,50, 'platform');
+        plat2 = game.add.sprite(200,200, 'platform');
+        plat3 = game.add.sprite(600,250, 'platform');
+        
+        police.body.collidesWorldBounds = true;
+        police.animation.add('walkRight', [0,1,2]);
+        police.animation.add('walkLeft', [3,4,5]);
+        
+        game.physics.arcade.enable(police);
+        game.physics.arcade.enable(plat1);
+        game.physics.arcade.enable(plat2);
+        game.physics.arcade.enable(plat3);
+        
+        cursors = game.input.keyboard.createCursorKeys();
+        
+        
+        plat1.body.static = true;
+        plat2.body.static = true;
+        plat3.body.static = true;
     }
 
     function update() {
-        heartText.setText("Hearts: " + Math.floor(heartTotal));
-        cursorText.setText(cursorCount);
-        cardText.setText(cardCount);
-        chocolateText.setText(chocolateCount);
-        roseText.setText(roseCount);
-        cursorText2.setText(cursorPrice);
-        cardText2.setText(cardPrice);
-        chocolateText2.setText(chocolatePrice);
-        roseText2.setText(rosePrice);
-        loveText.setText("Show the Kittens your love: " + love);
+        if(cursors.left.isDown){
+            police.animation.play('walkLeft', 10);
+            police.body.velocity.x = -350;
+        }
+        else if(cursors.right.isDown){
+            police.animation.play('walkRight', 10);
+            police.body.velocity.x = 350
+        }
+        else{
+            police.body.velocity.x = 0;
+        }
         
-        if (love > 200){
-            game.add.image(0,0, 'lose');
+        if(cursors.up.isDown && police.body.onFloor()){
+            police.body.velocity.y = -250;
         }
     }
 };
