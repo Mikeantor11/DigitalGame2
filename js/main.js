aa122adf402fe3ebf6cdfba99ec293615644d48f
Michael Hudson <mhudson8@gmu.edu>
2015-02-17 11:23:13 -0500

    comment it out

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ window.onload = function () {
         plat2 = game.add.sprite(200,200, 'platform');
         plat3 = game.add.sprite(600,250, 'platform');
         
-        police.body.collideWorldBounds = true;
+        //police.body.collideWorldBounds = true;
         police.animation.add('walkRight', [0,1,2]);
         police.animation.add('walkLeft', [3,4,5]);
         
