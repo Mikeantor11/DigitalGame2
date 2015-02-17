dcaa8c7463055e9dd429a401ddec4bc8f7e93c4f
Michael Hudson <mhudson8@gmu.edu>
2015-02-17 12:36:01 -0500

    Try this?

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ window.onload = function () {
         game.physics.arcade.gravity.y = 200;
         
         police = game.add.sprite(100,533, 'police');
-        plat1 = game.add.spirte(75,550, 'platform');
+        plat1 = game.add.image(75,550, 'platform');
         plat2 = game.add.sprite(200,450, 'platform');
         plat3 = game.add.sprite(500,400, 'platform');
         
@@ -44,7 +44,7 @@ window.onload = function () {
         
         police.animations.add('walkRight', [0,1,2]);
         police.animations.add('walkLeft', [3,4,5]);
-        plat1.body.moves = false;
+        //plat1.body.moves = false;
         plat2.body.moves = false;
         plat3.body.moves = false;
         
