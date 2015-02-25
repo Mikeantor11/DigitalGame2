a7f4ab0a21f3a38e1dcaa99782c005c20ae71d7f
Michael Hudson <mhudson8@gmu.edu>
2015-02-24 21:15:42 -0500

    Changing game

--- js/main.js
+++ js/main.js
@@ -16,16 +16,23 @@ window.onload = function () {
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {preload: preload, create: create, update: update});
 
     function preload() {
-        game.load.image('background', '');
+        game.load.image('background', 'assets/Art/birdInterScene.png');
+        game.load.image('noticed', 'assets/Art/noticedInter.png');
+        game.load.image('almost', 'assets/Art/almostcaughtInter.png');
+        game.load.image('caught', 'assets/Art/caughtInter.png');
     }
 	
 	var question;
-	var cont;
+	var seen;
+        var job;
 	var opt1;
 	var opt2;
 	var opt3;
+        var style;
+        var cursors;
 	var waitResp = false;
 	var progress = 0;
+        var move;
 	
     function create() {
         
@@ -37,28 +44,47 @@ window.onload = function () {
         cursors = game.input.keyboard.createCursorKeys();
     }
 
-    function collision(){
-        counter++;
-    }
-    
-    function win(){
-        game.add.text(100,100, "You Win!!!", style);
-    }
-
     function update() {
         if(game.input.keyboard.isDown(Phaser.Keyboard.SPACE) && !waitResp){
             progress++;
         }
         
         if(progress === 1){
-        	question.setText("Tell me a little about yourself.");
-        	move.setText("");
-        	opt1 = game.add.text(50, 450, "1) I'm a hard worker who likes to spend his free time working for the company.", style);
-        	opt2 = game.add.text(50, 465, "2) I always get my work done in a timely manner, in my free time I like socializing with frineds.", style);
-        	opt3 = game.add.text(50, 480, "3) I am from the Underworld hear to enslave the entire human race!", style);
-        	while(true){
-        		if(game.input.keyboard.isDown(Phaser.Keyboard.
-        	}
+            question.setText("Tell me a little about yourself.");
+            move.setText("");
+            opt1 = game.add.text(50, 450, "1) I'm a hard worker who likes to spend his free time working for the company.", style);
+            opt2 = game.add.text(50, 465, "2) I always get my work done in a timely manner, in my free time I like socializing with frineds.", style);
+            opt3 = game.add.text(50, 480, "3) I am from the Underworld hear to enslave the entire human race!", style);
+            while(true){
+                if(game.input.keyboard.isDown(Phaser.Keyboard.ONE)){
+                    job = job + 2;
+                    if(seen > 0){
+                        seen--;
+                    }
+                }
+                else if(game.input.keyboard.isDown(Phaser.Keyboard.TWO)){
+                    job = job + 1;
+                    if(seen === 1){
+                        seen--;
+                    }
+                }
+                else if(game.input.keyboard.isDown(Phaser.Keyboard.THREE)){
+                    job--;
+                    seen++;
+                }
+            }
+            
+            progress++;
+        }
+        
+        else if(progress === 2){
+            if(seen > 0){
+                question.setText("Riight... let's move on.");
+                move.setText("Press SPACE to continue.");
+                opt1.setText("");
+                opt2.setText("");
+                opt3.setText("");
+            }
         }
     }
 };
