e408808c2e2c7ca66fe3daac50e8b44ce8e52fdc
Michael Hudson <mhudson8@gmu.edu>
2015-03-18 20:50:23 -0400

    Change Games

--- js/main.js
+++ js/main.js
@@ -23,223 +23,158 @@ window.onload = function () {
         game.load.spritesheet("sheet", 'assets/Art/birdInterSpriteSheet.png', 800, 600);
     }
 	
-    var question;
-    var seen = 0;
-    var job = 0;
+    var instructions;
     var opt1;
-    var opt2;
-    var opt3;
     var style;
     var cursors;
-    var waitResp = false;
-    var progress = 0;
     var move;
-    var resp = false;
     var background;
+    var numbers = [];
+    var response = [];
+	var progress = 1;
+	var generate = true;
+	var show = false;
+	var wait = false;
+	var input = false;
+	var compare = false;
+	var diplay;
+	var random = new RandomDataGenerator();
+	var 0key;
+	var 1key;
+	var 2key;
+	var 3key;
+	var 4key;
+	var 5key;
+	var 6key;
+	var 7key;
+	var 8key;
+	var 9key;
+	var enter;
+	var counter = 0;
 	
     function create() {
         
         style = {font: "13px Arial", fill: "#FFFFFF", align: "center"};
         background = game.add.sprite(0, 0, 'sheet');
-        question = game.add.text(50, 420, 'Welcome to the interview.', style);
-        move = game.add.text(75, 435, 'Press Down to continue', style);
-        opt1 = game.add.text(50, 450, "", style);
-        opt2 = game.add.text(50, 465, "", style);
-        opt3 = game.add.text(50, 480, "", style);
+        instructions = game.add.text(50, 20, 'Remember the Numbers shown!', style);
+        move = game.add.text(75, 35, 'Press Down to continue', style);
+        opt1 = game.add.text(50, 50, "", style);
         
         cursors = game.input.keyboard.createCursorKeys();
+        0key = game.input.keyboard.addKey(Phaser.Keyboard.ZERO);
+        1key = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+        2key = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
+        3key = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
+        4key = game.input.keyboard.addKey(Phaser.Keyboard.FOUR);
+        5key = game.input.keyboard.addKey(Phaser.Keyboard.FIVE);
+        6key = game.input.keyboard.addKey(Phaser.Keyboard.SIX);
+        7key = game.input.keyboard.addKey(Phaser.Keyboard.SEVEN);
+        8key = game.input.keyboard.addKey(Phaser.Keyboard.EIGHT);
+        9key = game.input.keyboard.addKey(Phaser.Keyboard.NINE);
+        enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        
     }
 
     function update() {
     
         background.frame = seen;
-        
-    	if(progress === 0){
-    		if(cursors.down.isDown){
-    			progress++;
-    		}
-    	}
-        
-        if(progress === 1){
-            question.setText("Tell me a little about yourself.");
-            move.setText("");
-            opt1.setText("Left) I'm a hard worker who likes to spend his free time working for the company.");
-            opt2.setText("Up) I always get my work done in a timely manner, in my free time I like socializing with friends.");
-            opt3.setText("Right) I AM FROM THE UNDERWORLD HERE TO ENSALVE THE ENTIRE HUMAN RACE!");
-            resp = true;
-                if(cursors.left.isDown){
-                    job = job + 2;
-                    if(seen > 0){
-                        seen--;
-                    }
-                    progress++;
-                }
-                else if(cursors.up.isDown){
-                    job = job + 1;
-                    if(seen === 1){
-                        seen--;
-                    }
-                    progress++;
-                }
-                else if(cursors.right.isDown){
-                    job--;
-                    seen++;
-                    progress++;
-                }
-        }
-        
-        else if(progress === 2){
-            if(seen > 0){
-                question.setText("Riight... let's move on.");
-                move.setText("Press Down to continue.");
-                opt1.setText("");
-                opt2.setText("");
-                opt3.setText("");
-            }
-            else{
-            	question.setText("That's great! Let's go to the next question.");
-            	move.setText("Press Down to continue.");
-            	opt1.setText("");
-            	opt2.setText("");
-            	opt3.setText("");
-            }
-            if(cursors.down.isDown){
-            	progress++;
-            }
-        }
-        
-        else if(progress === 3){
-        	question.setText("What are some of your strengths?");
-            move.setText("");
-            opt1.setText("Left) I'm a great team player who enjoys working in groups or alone.");
-            opt2.setText("Up) I manage my time pretty well.");
-            opt3.setText("Right) THE FIRE THAT BURNS FROM MY HEART FOR TOTAL DOMINATION!");
-            resp = true;
-                if(cursors.left.isDown){
-                    job = job + 2;
-                    if(seen > 0){
-                        seen--;
-                    }
-                    progress++;
-                }
-                else if(cursors.up.isDown){
-                    job = job + 1;
-                    if(seen === 1){
-                        seen--;
-                    }
-                    progress++;
-                }
-                else if(cursors.right.isDown){
-                    job--;
-                    seen++;
-                    progress++;
-                }
-        }
-        
-        else if(progress === 4){
-            if(seen > 1){
-            	question.setText("Yeaaaaaah.... let's go with that.");
-            	move.setText("Press Down to continue.");
-            	opt1.setText("");
-            	opt2.setText("");
-            	opt3.setText("");
-            }
-            else if(seen > 0){
-                question.setText("Riight... let's move on.");
-                move.setText("Press Down to continue.");
-                opt1.setText("");
-                opt2.setText("");
-                opt3.setText("");
-            }
-            else{
-            	question.setText("That's great! Let's go to the next question.");
-            	move.setText("Press Down to continue.");
-            	opt1.setText("");
-            	opt2.setText("");
-            	opt3.setText("");
-            }
-            
-            if(cursors.down.isDown){
-            	progress++;
-            }
-        }
-        
-        else if(progress === 5){
-        	question.setText("What are some of your weaknesses?");
-            move.setText("");
-            opt1.setText("Left) I tend to work to much some of the times.");
-            opt2.setText("Up) Sometimes I get overwhelmed with my work.");
-            opt3.setText("Right) THE PUNY HUMANS UNDER MY FEET!");
-            resp = true;
-                if(cursors.left.isDown){
-                    job = job + 2;
-                    if(seen > 0){
-                        seen--;
-                    }
-                    progress++;
-                }
-                else if(cursors.up.isDown){
-                    job = job + 1;
-                    if(seen === 1){
-                        seen--;
-                    }
-                    progress++;
-                }
-                else if(cursors.right.isDown){
-                    job--;
-                    seen++;
-                    progress++;
-                }
-        }
-        
-        else if(progress === 6){
-        	if(seen > 2){
-        		question.setText("Ummm.... we're gonna need you to come with us.");
-        		move.setText("");
-        		opt1.setText("");
-        		opt2.setText("");
-        		opt3.setText("");
+
+			opt1.setText("Press the corresponding numbers in order.");
+			if(cursors.down.isDown){
+			if(generate){
+				numbers = [];
+				for(int i = 0; i < progress; i++){
+        			numbers[i] = random.between(0,9);
+        		}
+        		response = [];
+        		generate = false;
+        		show = true;
         	}
-            else if(seen > 1){
-            	question.setText("Yeaaaaaah.... let's go with that.");
-            	move.setText("Press Down to continue.");
-            	opt1.setText("");
-            	opt2.setText("");
-            	opt3.setText("");
-            }
-            else if(seen > 0){
-                question.setText("Riight... let's move on.");
-                move.setText("Press Down to continue.");
-                opt1.setText("");
-                opt2.setText("");
-                opt3.setText("");
-            }
-            else{
-            	question.setText("That's great! Let's move on.");
-            	move.setText("Press Down to continue.");
-            	opt1.setText("");
-            	opt2.setText("");
-            	opt3.setText("");
-            }
-            
-            if(cursors.down.isDown){
-            	progress++;
-            }
+        	}
+        	
+        	if (show){
+        		for(int i = 0; i < numbers.length; i++){
+        			display = display + numbers[i].toString + ' ';
+        		}
+        		opt1.setText(display);
+        		show  = false;
+        		wait = true;
+        	}
+        	
+        	if(wait){
+        		if(cursors.down.isDown){
+        			wait = false;
+        			input = true;
+        		}
+        	}
+	   	
+        while(input){
+        	if(0key.isDown){
+        		response[counter] = 0;
+        		counter++;
+        	}
+        	
+        	if(1key.isDown){
+        		response[counter] = 1;
+        		counter++;
+        	}
+        	
+        	if(2key.isDown){
+        		response[counter] = 2;
+        		counter++;
+        	}
+        	
+        	if(3key.isDown){
+        		response[counter] = 3;
+        		counter++;
+        	}
+        	
+        	if(4key.isDown){
+        		response[counter] = 4;
+        		counter++;
+        	}
+        	
+        	if(5key.isDown){
+        		response[counter] = 5;
+        		counter++;
+        	}
+        	
+        	if(6key.isDown){
+        		response[counter] = 6;
+        		counter++;
+        	}
+        	
+        	if(7key.isDown){
+        		response[counter] = 7;
+        		counter++;
+        	}
+        	
+        	if(8key.isDown){
+        		response[counter] = 8;
+        		counter++;
+        	}
+        	
+        	if(9key.isDown){
+        		response[counter] = 9;
+        		counter++;
+        	}
+        	
+        	if(enter.isDown){
+        		input = false;
+        		compare = true;
+        	}
         }
         
-        else if(progress === 7){
-        	if(job > 5){
-        		question.setText("Well we really like you and would like to offer you a job, you start tomorrow!");
-        		move.setText("Congratulations!");
+        if(compare){
+        	if(response = numbers){
+        		opt1.setText("Congratulations!");
+        		generate = true;
         	}
-        	else if(job > 2){
-        		question.setText("Well thank you for your time. We will be contacting you within the week.");
-        		move.setText("Good Work!");
-        	}
+        	
         	else{
-        		question.setText("Well ummm... thank you for your time. Don't call us we'll call you.");
-        		move.setText("In case you didn't know...you didn't get the job.");
+        		opt1.setText("I'm sorry you got it wrong :(");
         	}
+        	compare  = false;
         }
     }
 };
